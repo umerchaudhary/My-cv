1b18f9b01531e1c02f54903a39075b2cdd825df5
umerchaudhary <itsumerch6432@gmail.com>
2023-11-17 00:28:24 +0500

    added code

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,67 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const profile = {
+    name: 'Muhammad Umer',
+    title: 'Full Stack Web Developer',
+    description: 'Passionate about creating web applications that make a difference.',
+    education: [
+      { degree: 'Bachelor of Science in Computer Science', school: 'University A', year: '2015-2019' },
+      { degree: 'Master of Science in Software Engineering', school: 'University B', year: '2020-2022' },
+      { degree: 'Web Development Bootcamp', school: 'Coding Academy', year: '2022' },
+    ],
+    skills: [
+      { skill: 'JavaScript', level: 80 },
+      { skill: 'React', level: 75 },
+      { skill: 'Node.js', level: 70 },
+      { skill: 'HTML', level: 85 },
+      { skill: 'CSS', level: 80 },
+      { skill: 'MongoDB', level: 75 },
+      { skill: 'Express', level: 70 },
+    ],
+    // Add more skills as needed
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="app">
+      <div className="left-column">
+        <img src="assets/image.jpeg" alt="Umer" />
+        <h2>Skills</h2>
+        <ul className="skills-list">
+          {profile.skills.map((skill, index) => (
+            <li key={index}>
+              <span className="skill-name">{skill.skill}</span>
+              <div className="progress-bar" style={{ width: `${skill.level}%` }}></div>
+            </li>
+          ))}
+        </ul>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
 
-export default App
+      <div className="right-column">
+        <header>
+          <h1>{profile.name}</h1>
+          <p>{profile.title}</p>
+        </header>
+
+        <section className="profile">
+          <p>{profile.description}</p>
+        </section>
+
+        <section className="education">
+          <h2>Education</h2>
+          <ul>
+            {profile.education.map((edu, index) => (
+              <li key={index}>
+                <strong>{edu.degree}</strong> - {edu.school} ({edu.year})
+              </li>
+            ))}
+          </ul>
+        </section>
+      </div>
+    </div>
+  );
+};
+
+export default App;
+
